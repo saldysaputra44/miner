1e6376a45911cd9f5d02a2ae12f72b1eef6330bb
Saldy Saputra <84979367+saldysaputra44@users.noreply.github.com>
2021-05-28 21:33:46 +0700

    Create xhv.sh

---
+++ xhv.sh
@@ -0,0 +1,7 @@
+# HavenProtocol
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz
+ls
+tar xf xmrig-6.10.0-linux-x64.tar.gz
+ls
+./xmrig-6.10.0/xmrig -a cn-heavy/xhv -o sg.haven.herominers.com:10451 -u hvs1W9dGuCb8PDpTtW3R7pfBvv15YhEqQcUZm4mmAXen6DYASem8QLiDLcZxVrcxkFUpuv9PbtiKUXAEkjsgDh9Y1jotwpUwW2.5000 -k --tls -p VPS --cpu 6
